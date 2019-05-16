bb39da26c553ba46aa061f0b640f7eeaa8eed383
plzthink123 <554382072@qq.com>
2019-05-16 10:22:47 +0800

    报销单持久层编写 填写报销单 个人报销单 修改报销单 **审核报销单**

---
+++ oa_biz/src/main/java/com/think123/oa/biz/ClaimVoucherBiz.java
@@ -0,0 +1,23 @@
+package com.think123.oa.biz;
+
+import com.think123.oa.entity.ClaimVoucher;
+import com.think123.oa.entity.ClaimVoucherItem;
+import com.think123.oa.entity.DealRecord;
+
+import java.util.List;
+
+public interface ClaimVoucherBiz {
+
+    void save(ClaimVoucher claimVoucher, List<ClaimVoucherItem> items);
+
+    ClaimVoucher get(int id);
+    List<ClaimVoucherItem> getItems(int cvid);
+    List<DealRecord> getRecords(int cvid);
+
+    List<ClaimVoucher> getForSelf(String sn);
+    List<ClaimVoucher> getForDeal(String sn);
+
+    void update(ClaimVoucher claimVoucher, List<ClaimVoucherItem> items);
+    void submit(int id);
+    void deal(DealRecord dealRecord);
+}
---
+++ oa_dao/src/main/java/com/think123/oa/dao/DealRecordDao.java
@@ -0,0 +1,11 @@
+package com.think123.oa.dao;
+
+import com.think123.oa.entity.DealRecord;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+@Repository("dealRecordDao")
+public interface DealRecordDao {
+    void insert(DealRecord dealRecord);
+    List<DealRecord> selectByClaimVoucher(int cvid);
+}
